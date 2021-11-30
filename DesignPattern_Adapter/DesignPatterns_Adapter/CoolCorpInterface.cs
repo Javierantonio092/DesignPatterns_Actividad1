a7e69395ca8fab74c3aa96d13caccb068ea7040d
unknown <rojasbarreragabriel@gmail.com>
2021-11-30 16:22:43 -0600

    add Adapter

---
+++ DesignPattern_Adapter/DesignPatterns_Adapter/CoolCorpInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_Adapter
+{
+    interface CoolCorpInterface
+    {
+        float CurrentSpeedInKilometersPerHour { get; set; }
+        void Jump(float meters);
+    }
+}
---
+++ DesignPattern_Adapter/DesignPatterns_Adapter/Program.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatterns_Adapter;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CoolCorpInterface robot = new CoolCorpRobot();
+        robot.CurrentSpeedInKilometersPerHour = 3;
+        robot.Jump(12);
+        Console.ReadKey();
+    }
+
+}
+
